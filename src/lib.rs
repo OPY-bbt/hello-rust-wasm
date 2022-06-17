a9781f643eae2463195df4f7d2d3a7b1cdbfc74d
zhangyan <zhyabs@gmail.com>
2022-06-17 16:07:27 +0800

    feat: add test

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,16 @@ impl Universe {
         self.height
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
     }
@@ -95,12 +105,7 @@ impl Universe {
         let height = 64;
 
         let cells = (0..width * height)
-            .map(|i| {
-                // if i % 2 == 0 || i % 7 == 0 {
-                //     Cell::Alive
-                // } else {
-                //     Cell::Dead
-                // }
+            .map(|_| {
                 if js_sys::Math::random() < 0.5 {
                     Cell::Alive
                 } else {
@@ -118,6 +123,19 @@ impl Universe {
 
     pub fn render(&self) -> String {
         self.to_string()
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
 
